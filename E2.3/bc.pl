816d46d607fb2a3a839f7be78854712c319f401e
diego-ornelas4952 <diego.ornelas2005>
2026-03-07 17:30:06 -0600

    fix -> Arreglé unos errores que había principalmente en la función de buscar, pero creo que ya está funcionando bien

--- E2.3/bc.pl
+++ E2.3/bc.pl
@@ -1,7 +1,8 @@
 :- dynamic recomendacion/2.
 
-recomendacion(series, ['Black Mirror','Altered Carbon','Westworld','Black Mirror']).
-recomendacion(musica, ['Synthwave','Cyberpunk OST','Lo-Fi']).
-recomendacion(libros, ['Neuromancer','Snow Crash','I Robot']).
-recomendacion(podcasts, ['Lex Fridman','Data Skeptic','AI Today']).
-recomendacion(peliculas, ['The Matrix','Inception','Blade Runner','Avatar']).
+recomendacion(musica,['Metallica','Eagle','Michael Jackson']).
+recomendacion(libros,['Neuromancer','Snow Crash','I Robot']).
+recomendacion(podcasts,['Lex Fridman','Data Skeptic','AI Today']).
+recomendacion(peliculas,['The Matrix','Inception','Blade Runner','Avatar']).
+recomendacion(juegos,[mario]).
+recomendacion(series,['Black Mirror','Altered Carbon','Westworld','Severance']).
--- E2.3/main.pl
+++ E2.3/main.pl
@@ -1,17 +1,24 @@
 :- use_module(library(lists)). % Para usar funciones de listas
 
+:- dynamic recomendacion/2.
+
 :- initialization(load_bc). % Carga la base de conocimiento al iniciar
 
 % Predicados para la base de conocimiento
 % Carga de la base del conocimiento
-load_bc :- 
+load_bc :-
     exists_file('bc.pl'),
-    consult('bc.pl'),
+    consult('bc.pl').
 load_bc :-
     writeln('Error al cargar la base de conocimiento. Crear al guardar').
 
+% Mostrar categorías
+show_cats :-
+    writeln('\nCategorias disponibles:'),
+    forall(recomendacion(Categoria, _), format('- ~w~n', [Categoria])).
+
 % Guardar la base del conocimiento
-guardar_bc :- 
+guardar_bc :-
     open('bc.pl', write, Stream),
     write(Stream, ':- dynamic recomendacion/2.\n\n'),
     forall(recomendacion(Categoria, Lista),
@@ -23,124 +30,141 @@ guardar_bc :-
 % Predicados para el menu
 % Buscar elemento
 buscar_elem(Categoria, Elemento) :-
-    recomendacion(Categoria, Lista),
-    (member (Elemento, Lista) 
-    -> format('Elemento encontrado: ~q\n', [Elemento, Categoria])
-    ; format('Elemento no encontrado, ¿Deseas agregarlo? (s/n)\n', [Elemento, Categoria]),
-    read(Resp),
-    (Resp == s -> agregar_elem(Categoria, Elemento) ; writeln('Operación cancelada'))).
+    ( recomendacion(Categoria, Lista)
+    -> ( member(Elemento, Lista)
+        -> format('Elemento ~q encontrado en la categoria ~q\n', [Elemento, Categoria])
+        ;  format('Elemento ~q no encontrado en ~q, ¿Deseas agregarlo? (s/n)\n', [Elemento, Categoria]),
+           read(Resp),
+           ( Resp == s -> add_elem(Categoria, Elemento) ; true )
+       )
+    ;  format('La categoría ~q no existe. ¿Deseas crearla y agregar el elemento? (s/n)\n', [Categoria]),
+       read(Resp),
+       ( Resp == s -> add_elem(Categoria, Elemento) ; writeln('Operación cancelada') )
+    ).
 
 % Listar elementos de una categoría
 listar_elem([]).
-listar_elem([H|T]) :- 
+listar_elem([H|T]) :-
     format('- ~w~n', [H]),
     listar_elem(T).
 
 % Comprobar lista
-comprobar_lista(Categoria) :- 
-    recomendacion(Categoria, Lista),
-    format('~nRecomendaciones para ~w:~n', [Categoria]),
-    listar_elem(Lista).
+comprobar_lista(Categoria) :-
+    (recomendacion(Categoria, Lista)
+    -> format('~nRecomendaciones para ~w:~n', [Categoria]),
+       listar_elem(Lista)
+    ; format('La categoría ~w no existe~n', [Categoria])).
 
 % Concatenar dos categorías
-demo_concat(cat1, cat2) :-
-    recomendacion(cat1, Lista1),
-    recomendacion(cat2, Lista2),
-    concat_propia(L1, L2, LP),
-    append(L1, L2, LAp),
-    format('Concatenación propia: ~q~n', [LP]),
-    format('Concatenación append: ~q~n', [LAp]).
+concat_propia([], L, L).
+concat_propia([H|T], L2, [H|L3]) :-
+    concat_propia(T, L2, L3).
+
+demo_concat(Cat1, Cat2) :-
+    (recomendacion(Cat1, Lista1), recomendacion(Cat2, Lista2)
+    -> concat_propia(Lista1, Lista2, LP),
+       append(Lista1, Lista2, LAp),
+       format('Concatenación propia: ~q~n', [LP]),
+       format('Concatenación append: ~q~n', [LAp])
+    ; writeln('Una o ambas categorías no existen.')).
 
 % Agregar elemento
 add_elem(Categoria, Elemento) :-
     (recomendacion(Categoria, Lista)
     -> retract(recomendacion(Categoria, Lista)),
-    append(Lista, [Elemento], NuevaLista);
-    newList = [Elemento]),
-    
-    assertz(recomendacion(Categoria, NuevaLista)), % 
+    append(Lista, [Elemento], NuevaLista)
+    ; NuevaLista = [Elemento]),
+
+    assertz(recomendacion(Categoria, NuevaLista)),
     guardar_bc,
     writeln('Elemento agregado con exito').
 
 % Eliminar elemento
 del_elem(Categoria, Elemento) :-
     (recomendacion(Categoria, Lista)
-    -> select(Elemento, Lista, NuevaLista),
-    retract(recomendacion(Categoria, Lista)),
-    assertz(recomendacion(Categoria, NuevaLista)),
-    guardar_bc,
-    writeln('Elemento eliminado con exito')
-    ; writeln('Elemento no encontrado')).
+    -> (select(Elemento, Lista, NuevaLista)
+        -> retract(recomendacion(Categoria, Lista)),
+           assertz(recomendacion(Categoria, NuevaLista)),
+           guardar_bc,
+           writeln('Elemento eliminado con exito')
+        ;  writeln('Elemento no encontrado en la lista'))
+    ; writeln('Categoría no encontrada')).
 
 % Obtener tamaño de una categoría
 size_list([], 0).
-size_list([_|T], N) :- 
-    size_list(T, N1), 
+size_list([_|T], N) :-
+    size_list(T, N1),
     N is N1 + 1.
 
 % Mostrar tamaño de una categoría
 show_size(Categoria) :-
-    recomendacion(Categoria, Lista),
-    size_list(Lista, N),
-    format('~nRecomendaciones para ~w: ~q~n', [Categoria, N]).
+    (recomendacion(Categoria, Lista)
+    -> size_list(Lista, N),
+       format('~nRecomendaciones para ~w: ~q~n', [Categoria, N])
+    ; format('La categoría ~w no existe~n', [Categoria])).
 
 % Ordenar lista
 ord_lista(Categoria) :-
-    recomendacion(Categoria, Lista),
-    sort(Lista, listaSort),
-    msort(Lista, listaMsort),
-    format('~nRecomendaciones ordenadas (Sort) para ~w: ~q~n', [listaSort]),
-    format('~nRecomendaciones ordenadas (Msort) para ~w: ~q~n', [listaMsort]),
+    (recomendacion(Categoria, Lista)
+    -> sort(Lista, ListaSort),
+       msort(Lista, ListaMsort),
+       format('~nRecomendaciones ordenadas (Sort) para ~w: ~q~n', [Categoria, ListaSort]),
+       format('~nRecomendaciones ordenadas (Msort) para ~w: ~q~n', [Categoria, ListaMsort])
+    ; format('La categoría ~w no existe~n', [Categoria])).
 
 % Menu
 menu :-
     writeln('\n--- Menu ---\n'),
-    writeln('1. Buscar elemento'),
-    writeln('2. Listar elementos de categoría'),
-    writeln('3. Concatenar dos categorías'),
-    writeln('4. Agregar elemento'),
-    writeln('5. Eliminar elemento'),
-    writeln('6. Obtener tamaño de una categoría'),
-    writeln('7. Ordenar lista'),
-    writeln('8. Salir'),
+    writeln('1. Mostrar categorías'),
+    writeln('2. Buscar elemento'),
+    writeln('3. Listar elementos de categoría'),
+    writeln('4. Concatenar dos categorías'),
+    writeln('5. Agregar elemento'),
+    writeln('6. Eliminar elemento'),
+    writeln('7. Obtener tamaño de una categoría'),
+    writeln('8. Ordenar lista'),
+    writeln('9. Salir'),
     write('Seleccione una opción: '), 
     read(Op),
     exe_op(Op).
 
 exe_op(1) :- 
+    show_cats, menu.
+
+exe_op(2) :- 
     write('Ingrese la categoría: '), 
     read(C), write('Ingrese el elemento: '), read(E),
     buscar_elem(C, E), menu.
 
-exe_op(2) :- 
+exe_op(3) :- 
     write('Ingrese la categoría: '), 
     read(C), 
     comprobar_lista(C), menu.
 
-exe_op(3) :- 
+exe_op(4) :- 
     write('Ingrese la categoría 1: '), 
     read(C1), write('Ingrese la categoría 2: '), read(C2),
     demo_concat(C1, C2), menu.
 
-exe_op(4) :- 
+exe_op(5) :- 
     write('Ingrese la categoría: '), 
     read(C), write('Ingrese el elemento que quiere añadir: '), read(E),
     add_elem(C, E), menu.
 
-exe_op(5) :- 
+exe_op(6) :- 
     write('Ingrese la categoría: '), 
     read(C), write('Ingrese el elemento que quiere eliminar: '), read(E),
     del_elem(C, E), menu.
 
-exe_op(6) :- 
+exe_op(7) :- 
     write('Ingrese la categoría: '), 
     read(C), show_size(C), menu.
 
-exe_op(7) :- 
+exe_op(8) :- 
     write('Ingrese la categoría: '), 
     read(C), ord_lista(C), menu.
 
-exe_op(8) :- 
+exe_op(9) :- 
     writeln('Bye!').
 
 exe_op(_) :- 
