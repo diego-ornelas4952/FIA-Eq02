abba31d667e183822a3c98e04aac0b32934ab027
diego-ornelas4952 <diego.ornelas2005>
2026-03-06 18:51:27 -0600

    feat -> Añadí datos a la base de conocimiento

---
+++ E2.3/bc.pl
@@ -0,0 +1,7 @@
+:- dynamic recomendacion/2.
+
+recomendacion(series, ['Black Mirror','Altered Carbon','Westworld','Black Mirror']).
+recomendacion(musica, ['Synthwave','Cyberpunk OST','Lo-Fi']).
+recomendacion(libros, ['Neuromancer','Snow Crash','I Robot']).
+recomendacion(podcasts, ['Lex Fridman','Data Skeptic','AI Today']).
+recomendacion(peliculas, ['The Matrix','Inception','Blade Runner','Avatar']).
