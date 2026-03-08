7114d563bc1f2c9e09ec06d26af036c1139b807d
Emmanuel Murillo Picazo <150549841+DREMP08@users.noreply.github.com>
2026-03-07 18:33:25 -0600

    style -> Añadi todo el html visual para quee funcione con el programa

--- E2.3/bc.pl
+++ E2.3/bc.pl
@@ -1,8 +1,8 @@
 :- dynamic recomendacion/2.
 
 recomendacion(musica,['Metallica','Eagle','Michael Jackson']).
-recomendacion(libros,['Neuromancer','Snow Crash','I Robot']).
 recomendacion(podcasts,['Lex Fridman','Data Skeptic','AI Today']).
 recomendacion(peliculas,['The Matrix','Inception','Blade Runner','Avatar']).
 recomendacion(juegos,[mario]).
 recomendacion(series,['Black Mirror','Altered Carbon','Westworld','Severance']).
+recomendacion(libros,['Neuromancer','Snow Crash','I Robot',prince]).
